b1e132022571f1a524e4744dc45da6e934b09665
ayushchoudhary-3190 <a3506794@gmail.com>
2026-04-01 10:27:30 +0000

    Phase 4: Add components index file

---
+++ frontend/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Navbar } from './Navbar';
+export { default as Wallet } from './Wallet';
+export { default as SwapForm } from './SwapForm';
+export { default as PoolCard } from './PoolCard';
